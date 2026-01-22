75f266e85602c0757ce8786c166c6033b01fd971
Harjuna Tri Nugroho <harjuna@vioren.com>
2026-01-22 11:39:04 +0700

    fixing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -78,7 +78,7 @@ export default function Home() {
           <p>Memuat...</p>
         </div>
       ) : (
-        <ExpenseList expenses={expenses} />
+        <ExpenseList expenses={expenses} onExpenseUpdated={fetchExpenses} />
       )}
     </div>
   )
